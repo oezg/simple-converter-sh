1ce908f2d4b5c464f3d998cceeb14256b2ea1258
Özgür Yıldırım <ozguryenihayat@gmail.com>
2023-06-29 23:10:14 +0200

    read input and check for correct format

--- simple_converter.sh
+++ simple_converter.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Enter a definition"
+echo "Enter a definition:"
+read -a user_input
+
+template_definition='^[a-z]+_to_[a-z]+$'
+template_constant='^-?[0-9]+(\.[0-9]+)?$'
+
+if [ ${#user_input[@]} -ne 2 ]; then
+    echo The definition is incorrect!
+elif [[ ${user_input[0]} =~ $template_definition ]]; then
+    if [[ ${user_input[1]} =~ $template_constant ]]; then
+        echo The definition is correct!
+    else 
+        echo The definition is incorrect!
+    fi
+else
+    echo The definition is incorrect!
+fi
